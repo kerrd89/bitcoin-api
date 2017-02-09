303fefa9385e4218b649c5e2215d464b729e67a1
David Kerr <kerrd89@gmail.com>
2017-02-09 08:43:30 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './lib/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.js?$/, exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ]
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.json', '.jsx', '.css']
+  }
+};
