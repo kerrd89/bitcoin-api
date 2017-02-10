0c3600bd8a9b9c42f25fc886a7acb11c2abf8e61
David Kerr <kerrd89@gmail.com>
2017-02-10 12:27:06 -0700

    annotated code

--- server.js
+++ server.js
@@ -18,31 +18,32 @@ const pullTransactionHistory = (user, count) => {
   if (!user) return null;
   return axios.get(`https://blockchain.info/address/${user}?format=json&limit=${count}`);
 };
-
+//serve the index.html file from the root
 app.get(['/'], (request, response) => {
   fs.readFile(`${__dirname}/public/index.html`, (err, file) => {
     response.send(file);
   });
 });
-
+//serve the index.html file when a specific address has been specified
 app.get('/:token', (request, response) => {
   fs.readFile(`${__dirname}/public/index.html`, 'utf8', (err, file) => {
     response.send(file);
   });
 });
 
+//endpoint to pull data for a given token or address
 app.get('/api/:address', (request, response) => {
   pullTransactionHistory(request.params.address, 25)
     .then(r => response.send({ txs: r.data }))
     .catch(err => console.log(err));
 });
 
+//endpoint to pull exchange rates
 app.get('/api/rates/all', (request, response) => {
   axios.get('https://blockchain.info/ticker')
     .then(r => response.send({ rates: r.data }))
     .catch(err => console.log(err));
 });
-
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
